1556fa91cbb736a9d6d56470a4ccbc54160c61a4
zhaoxu-lingoace <xu.zhao@pplingo.com>
2023-06-07 16:13:33 +0800

    使用全局路径导入

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
 
 const app = createApp(App)
 
